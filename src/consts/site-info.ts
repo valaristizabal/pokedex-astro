a8a2c5e757737d9265465bfed548fd4127e4eec4
Valeria <05valarist@gmail.com>
2025-04-20 22:34:26 -0500

    feat: navegación de las tablas y en cada pokémon

---
+++ src/consts/site-info.ts
@@ -0,0 +1,4 @@
+export const siteInfo = {
+    title: "PokeDex",
+    description: "Un website estática para ver información de Pokémon"
+}
--- src/interfaces/pokemon-sprite.ts
+++
@@ -1,8 +0,0 @@
-export interface PokemonSpriteResponse {
-    official_artwork: OfficialArtwork[];
-}
-
-export interface OfficialArtwork {
-    front_default: string;
-    front_shiny: string;
-}
